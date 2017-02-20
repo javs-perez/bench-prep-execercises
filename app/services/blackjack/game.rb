b068df4b7a0244a84b87f62df597a07353e62bea
Javier Perez <perez.javi.a@gmail.com>
2017-02-20 13:32:53 -0500

    making sure Game class can keep track of at least one game

--- app/services/blackjack/game.rb
+++ app/services/blackjack/game.rb
@@ -2,12 +2,24 @@ module Blackjack
   class Game
     attr_reader :player_hand, :dealer_hand
 
+    @@instance_collector = []
+
     def initialize
+      # clearing class variable, at the moment it will only be able to keep track
+      # of one game
+      @@instance_collector.clear
+
+      # collecting instance to share between actions
+      @@instance_collector << self
       @deck = Deck.new
       @player_hand = Hand.new
       @dealer_hand = Hand.new
       2.times { @player_hand.hit!(@deck) }
       2.times { @dealer_hand.hit!(@deck) }
+    end
+
+    def self.all_instances
+      @@instance_collector
     end
 
     def hit
@@ -24,11 +36,13 @@ module Blackjack
     end
 
     def status
-      {:player_cards=> @player_hand.cards,
-       :player_value => @player_hand.value,
-       :dealer_cards => @dealer_hand.cards,
-       :dealer_value => @dealer_hand.value,
-       :winner => @winner}
+      {
+        :player_cards=> @player_hand.cards,
+        :player_value => @player_hand.value,
+        :dealer_cards => @dealer_hand.cards,
+        :dealer_value => @dealer_hand.value,
+        :winner => @winner
+    }
     end
 
     def determine_winner(player_value, dealer_value)
