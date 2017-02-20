6d35dc0bc0a000eac76b3d694f3eb4d43ee5b818
Javier Perez <perez.javi.a@gmail.com>
2017-02-20 13:32:36 -0500

    game controller actions and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :quotes, only: :index
   resources :array_searches, only: [:new, :create]
+  resources :blackjack_games, only: :new
+  get '/blackjack_game', to: 'blackjack_games#show'
+  post '/blackjack_games/hit', to: 'blackjack_games#hit'
+  post '/blackjack_games/stand', to: 'blackjack_games#stand'
 end
