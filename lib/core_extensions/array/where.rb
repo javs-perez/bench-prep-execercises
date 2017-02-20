8aacfa51584b025b8ff6f8a23f4ab78fb1097a8d
Javier Perez <perez.javi.a@gmail.com>
2017-02-20 14:25:59 -0500

    added tests for array where monkey patching

--- lib/core_extensions/array/where.rb
+++ lib/core_extensions/array/where.rb
@@ -3,7 +3,7 @@ module CoreExtensions
     module Where
       def where(query)
         self.select do |hash|
-          query.any? do |key, value|
+          query.all? do |key, value|
             if hash[key].is_a?(String)
               hash[key].match(value)
             else
