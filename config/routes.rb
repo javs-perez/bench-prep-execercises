d94e62259d7595573a7a447c4e78402a0cdcda12
Javier Perez <perez.javi.a@gmail.com>
2017-02-20 14:34:21 -0500

    new pages controller for guidance

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'pages#index'
   resources :quotes, only: :index
   resources :array_searches, only: [:new, :create]
   resources :blackjack_games, only: :new
